832c28f71538f7ed1bf5c4ef04eb1e9fe975a3d5
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-07-26 17:20:08 +0100

    Create selectedSong reducer with action.type SONG_SELECTED from action creator

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,3 +6,11 @@ const songsReducer = () => {
 		{ title: 'I Want It That Way', duration: '1:47' }
 	];
 };
+
+const selectedSongReducer = (selectedSong = null, action) => {
+	if (action.type === 'SONG_SELECTED') {
+		return action.payload;
+	}
+
+	return selectedSong;
+};
